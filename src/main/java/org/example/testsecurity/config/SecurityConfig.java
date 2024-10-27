5090f3840ec17c39f8cf0662b193615f878a9462
Jealyang <jaelyangchoi@gmail.com>
2024-10-27 16:10:25 +0900

    커스텀 로그인 설정

--- src/main/java/org/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/testsecurity/config/SecurityConfig.java
@@ -1,4 +1,36 @@
 package org.example.testsecurity.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/login").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated() //로그인한 사용자 접근 가능
+        );
+
+        http.formLogin((auth) -> auth
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .permitAll()
+        );
+
+        http.csrf((auth) -> auth.disable());
+
+
+
+        return http.build();
+    }
 }
--- src/main/java/org/example/testsecurity/controller/AdminController.java
+++ src/main/java/org/example/testsecurity/controller/AdminController.java
@@ -1,4 +1,13 @@
 package org.example.testsecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+        return "admin";
+    }
 }
