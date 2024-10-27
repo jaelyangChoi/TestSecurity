393faa9207a8fed209d7a04d097623760d45e16d
Jealyang <jaelyangchoi@gmail.com>
2024-10-27 16:13:01 +0900

    패스워드 Bcrypt 암호화

--- src/main/java/org/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/testsecurity/config/SecurityConfig.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(); // 패스워드 단방향 암호화 진행 시 사용
+    }
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
