b6dc7bded578756431d04c3ddd496cf44456b52a
Jealyang <jaelyangchoi@gmail.com>
2024-10-27 17:23:40 +0900

    세션 사용자 아이디 정보

--- src/main/java/org/example/testsecurity/controller/MainController.java
+++ src/main/java/org/example/testsecurity/controller/MainController.java
@@ -1,14 +1,29 @@
 package org.example.testsecurity.controller;
 
 import lombok.Getter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainP(){
+    public String mainP(Model model) {
+
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.iterator().next().getAuthority();
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
         return "main";
     }
 
